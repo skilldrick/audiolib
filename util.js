96c472211e6161724be007d1c0279da778d31dfa
Nick Morgan <skilldrick@gmail.com>
2016-03-12 11:52:28 -0800

    Clean up connect impl

--- util.js
+++ util.js
@@ -2,15 +2,13 @@
 const node = (input, output) => ({
   input: input,
   connect: (node) => output.connect(node)
-})
+});
+
+const connectNodes = (node1, node2) =>
+  node1.connect(node2 instanceof AudioNode ? node2 : node2.input);
 
 // Connect `nodes` together in a chain. Can be AudioNode or AudioNode-like
-const connect = (...nodes) => {
-  nodes.reduce((node1, node2) => {
-    node1.connect(node2 instanceof AudioNode ? node2 : node2.input);
-    return node2;
-  });
-}
+const connect = (...nodes) => nodes.reduce(connectNodes);
 
 const noteToFreq = (note) => 440 * Math.pow(2, note / 12);
 
@@ -19,6 +17,6 @@ const detune = (freq, percent) => {
   const amount = percent / 100;
   const detuneScalar = 1 + Math.random() * amount * 2 - amount;
   return freq * detuneScalar;
-}
+};
 
 module.exports = {connect, node, detune, noteToFreq};
