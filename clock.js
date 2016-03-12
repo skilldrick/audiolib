e19535ed8ac274b8d722e9fdbdc6e6dea49a7b33
Nick Morgan <skilldrick@gmail.com>
2016-03-11 23:55:27 -0800

    Improve clock code, handle backgrounding gracefully

--- clock.js
+++ clock.js
@@ -3,7 +3,9 @@ import {getCurrentTime} from './audio';
 class Clock {
   constructor() {
     this.beatLength = null;
-    this.timeoutTime = 50;
+    this.backgroundTimeoutTime = 1000;
+    this.foregroundTimeoutTime = 50;
+    this.timeoutTime = this.foregroundTimeoutTime;
     this.callbacks = new Set();
     this.playing = false;
   }
@@ -24,12 +26,11 @@ class Clock {
     if (this.playing) return;
     this.playing = true;
 
-    let previousTime = getCurrentTime();
     this.timeInBeats = 0; // Fractional beat count
     this.beat = 0;        // Integer beat count
 
     // Keeps timeInBeats up-to-date
-    const timeoutFunc = () => {
+    const timeoutFunc = (previousTime) => {
       const now = getCurrentTime();
       const diff = now - previousTime;
       const diffInBeats = diff / this.beatLength;
@@ -38,17 +39,35 @@ class Clock {
       this.tick(now, this.timeInBeats);
 
       if (this.playing) {
-        setTimeout(timeoutFunc, this.timeoutTime);
+        setTimeout(timeoutFunc.bind(null, now), this.timeoutTime);
       }
     };
 
-    timeoutFunc();
+    timeoutFunc(getCurrentTime());
   }
 
+  stop() {
+    this.playing = false;
+  }
 
+  /*
+  * tick is responsible for calling the callbacks for every beat once and only once.
+  * It calls the callbacks ahead of time, potentially triggering multiple beats
+  * in one go.
+  *
+  * The lookahead is kept as short as possible so we can react quickly to tempo changes,
+  * but if it's too short we'll skip beats. The optimum lookahead time is a function of
+  * the current timeout time and the beat length.
+  */
   tick(now, timeInBeats) {
+    this.checkIfBackgrounded();
     const thisBeat = this.beat;
-    const nextBeat = Math.ceil(timeInBeats) + 1; // Look ahead by 1 beat
+
+    // Calculate number of beats to look ahead by based on timeout time and beat length
+    const timeoutTimeInSeconds = this.timeoutTime / 1000;
+    const lookahead = (timeoutTimeInSeconds / this.beatLength) * 2;
+    // Math.max to ensure nextBeat can't be less than thisBeat
+    const nextBeat = Math.max(thisBeat, Math.ceil(timeInBeats + lookahead));
 
     // call callbacks for all beats between thisBeat and nextBeat
     for (let i = thisBeat; i < nextBeat; i++) {
@@ -61,7 +80,7 @@ class Clock {
   callCallbacks(beat, now, timeUntilBeat) {
     // If timeUntilBeat is negative, we weren't able to keep up
     if (timeUntilBeat < 0) {
-      console.error("Clock can't keep up, did you change tabs?");
+      console.warn("Clock skipped beats.");
       return;
     }
 
@@ -77,10 +96,20 @@ class Clock {
     }
   }
 
-  stop() {
-    this.playing = false;
+  /*
+  * This lets us know if the tab is backgrounded, and thus should
+  * increase the timeout time.
+  *
+  * It's possible that changing tabs will cause us to miss a beat,
+  * but after that things should get back on track.
+  */
+  checkIfBackgrounded() {
+    if (document.hidden) {
+      this.timeoutTime = this.backgroundTimeoutTime;
+    } else {
+      this.timeoutTime = this.foregroundTimeoutTime;
+    }
   }
-
 };
 
 module.exports = new Clock();
