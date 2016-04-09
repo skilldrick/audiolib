745a49ab2b4cb38758056770725e46170f15f87e
Nick Morgan <skilldrick@gmail.com>
2016-04-08 23:18:22 -0700

    Stop trying to make fetch happen

--- src/ajax.js
+++ src/ajax.js
@@ -1,8 +1,19 @@
 import { ctx } from './audio';
 
+const getData = (filename, cb) => {
+  return new Promise(resolve => {
+    const request = new XMLHttpRequest();
+    request.open('GET', filename, true);
+    request.responseType = 'arraybuffer';
+    request.onload = () => {
+      resolve(request.response);
+    };
+    request.send();
+  });
+}
+
 export default (filename) => new Promise((resolve, reject) => {
-  fetch(filename)
-    .then(response => response.arrayBuffer())
+  getData(filename)
     .then(audioData => ctx.decodeAudioData(audioData, resolve))
     .catch(reject);
 });
