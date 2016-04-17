c3e543b1fc1ac05891e954b68367fffadb7193f2
Nick Morgan <skilldrick@gmail.com>
2016-04-16 21:52:09 -0700

    Split Sampler into SingleBufferSampler and MultiBufferSampler

--- src/sampler.js
+++ src/sampler.js
@@ -2,7 +2,25 @@ import { ctx } from './audio';
 import { connect, Node } from './util';
 import { createBufferSource } from './nodes';
 
-export default class Sampler extends Node {
+// A SingleBufferSampler takes a buffer and a map of names to offsets
+export class SingleBufferSampler extends Node {
+  constructor(buffer, offsetMap) {
+    super();
+    this.buffer = buffer;
+    this.offsetMap = offsetMap;
+  }
+
+  play = (sampleName, when, length, playbackRate=1) => {
+    const source = createBufferSource(this.buffer, playbackRate);
+    connect(source, this.output);
+    const offset = this.offsetMap[sampleName];
+    source.start(when, offset, length);
+    return source;
+  }
+}
+
+// A MultiBufferSampler takes a map of names to buffers
+export class MultiBufferSampler extends Node {
   constructor(bufferMap) {
     super();
     this.bufferMap = bufferMap;
