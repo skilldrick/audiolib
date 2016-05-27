8298d99cb6d74ce7d7c18e8a391fe44e38c503f7
Nick Morgan <skilldrick@gmail.com>
2016-05-26 22:50:41 -0700

    Change Sampler interface

--- src/sampler.js
+++ src/sampler.js
@@ -1,29 +1,68 @@
-import { ctx } from './audio';
+import _ from 'lodash';
+
+import { ctx, getCurrentTime } from './audio';
 import { connect, Node } from './util';
-import { createBufferSource } from './nodes';
+import { createBufferSource, createGain } from './nodes';
 
 // A SingleBufferSampler takes a buffer and a map of names to offsets
 export class SingleBufferSampler extends Node {
   constructor(buffer, offsetMap) {
     super();
     this.buffer = buffer;
-    this.setOffsets(offsetMap);
+    this.setSampleMap(offsetMap);
   }
 
-  play = (sampleName, when, length, playbackRate=1) => {
-    const offset = this.offsetMap[sampleName];
-    this.playOffset(offset, when, length, playbackRate);
+  play = (sampleName, when) => {
+    const sample = this.sampleMap[sampleName];
+
+    return this.playOffset(
+      sample.offset,
+      when,
+      sample.length,
+      sample.playbackRate,
+      sample.fadeIn,
+      sample.fadeOut
+    );
   }
 
-  playOffset = (offset, when, length, playbackRate=1) => {
+  playOffset = (offset, when, length, playbackRate=1, fadeIn=0, fadeOut=0) => {
     const source = createBufferSource(this.buffer, playbackRate);
-    connect(source, this.output);
+    const envelope = this.createEnvelope(fadeIn, fadeOut, when, length);
+    connect(source, envelope, this.output);
     source.start(when, offset, length);
     return source;
   }
 
-  setOffsets(offsetMap) {
-    this.offsetMap = offsetMap;
+  createEnvelope = (fadeIn, fadeOut, when, length) => {
+    if (when === 0) {
+      when = getCurrentTime();
+    }
+
+    const releaseEnd = when + length;
+    const releaseStart = releaseEnd - fadeOut;
+    const attackEnd = Math.min(when + fadeIn, releaseStart);
+
+    const gain = createGain(0);
+    gain.gain.setValueAtTime(0, when);
+
+    gain.gain.linearRampToValueAtTime(1, attackEnd);
+    gain.gain.setValueAtTime(1, releaseStart);
+    gain.gain.linearRampToValueAtTime(0, releaseEnd);
+
+    return gain;
+  }
+
+  setSampleMap(offsetMap) {
+    this.sampleMap = _.mapValues(offsetMap, (value, key) => {
+      return {
+        offset: value,
+        name: key,
+        playbackRate: 1,
+        length: 0.2,
+        fadeIn: 0,
+        fadeOut: 0
+      };
+    });
   }
 }
 
