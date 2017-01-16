e5be7535570ccf3689c1ed73badf83943b5aefa4
Nick Morgan <skilldrick@gmail.com>
2017-01-15 16:53:09 -0800

    Add browserify

---
+++ src/browserify.js
@@ -0,0 +1,23 @@
+import * as ajax from './ajax';
+import * as audio from './audio';
+import * as clock from './clock';
+import * as fx from './fx';
+import * as logo from './logo';
+import * as nodes from './nodes';
+import * as sampler from './sampler';
+import * as scheduler from './scheduler';
+import * as synth from './synth';
+import * as util from './util';
+
+export {
+  ajax,
+  audio,
+  clock,
+  fx,
+  logo,
+  nodes,
+  sampler,
+  scheduler,
+  synth,
+  util
+};
